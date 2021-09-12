8b794f1b40119906ff0169535c728e1dd5bc907e
Mehedi Hasan Saikat <29018897+mehedi-hasan-saikat@users.noreply.github.com>
2021-09-12 23:20:47 +0600

    1st pic show bug fix

--- js/app.js
+++ js/app.js
@@ -10,7 +10,6 @@ loadProducts();
 const showProducts = (products) => {
   const allProducts = products.map((pd) => pd);
   for (const product of allProducts) {
-    console.log(product);
     const image = product.image;
     const div = document.createElement("div");
     div.classList.add("product");
